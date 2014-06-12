c6e82bcbefd4c588748cfe42854654c6ed5f141d
Björn Ebbinghaus <bjebb100@hhu.de>
2014-06-12 10:22:55 +0200

    Initial Commit

---
+++ C-project/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C-project
+//
+//  Created by Björn Ebbinghaus on 12.06.14.
+//  Copyright (c) 2014 Björn Ebbinghaus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
