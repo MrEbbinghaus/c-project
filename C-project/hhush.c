4be065a74790e19b74200b9126b66a739322a859
Björn Ebbinghaus <bjebb100@hhu.de>
2014-06-13 21:35:42 +0200

    switch to pointers

--- C-project/hhush.c
+++ C-project/hhush.c
@@ -1,9 +1,9 @@
 //
 //  hhush.c
-//  C-project
+//  C-project for the 
 //
-//  Created by Björn Ebbinghaus on 12.06.14.
-//  Copyright (c) 2014 Björn Ebbinghaus. All rights reserved.
+//  Created by Bjoern Ebbinghaus on 12.06.14.
+//  Copyright (c) 2014 Bjoern Ebbinghaus. All rights reserved.
 //
 
 #include <stdio.h>
@@ -24,10 +24,14 @@
 #include <dirent.h>
 #include <unistd.h>
 
-char* trimString(char[]);
+//functions
+char* trimString(char *);
+char* strsub(char *,int,int);
+int interpretCMD(char *,char *);
+int interpretIn(char *);
 
 
-int interpretCMD(char cmd[],char rest[]){
+int interpretCMD(char *cmd,char *rest){
     if(strcmp(cmd,"echo")==0){
         printf("%s\n",rest);
         return EXIT_SUCCESS;
@@ -39,16 +43,17 @@ int interpretCMD(char cmd[],char rest[]){
         return EXIT_SUCCESS;
     }
     else{printf("command not found\n");}
+
     return EXIT_FAILURE;
 }
 
-int interpretIn(char input[]){
-    input[strlen(input)-1]=0;
-    int cmdEnd = strcspn(input," ");
+int interpretIn(char *input){
+    long cmdEnd = strcspn(input," ");
+    
     if(cmdEnd==strlen(input)){}
-    char cmd[cmdEnd+1];
-    int paramL = strlen(input)-(cmdEnd+1); //-1 to eleminate \n , +1 to eleminate the ' '
-    char params[paramL+1];
+    char *cmd = malloc(cmdEnd+1);
+    long paramL = strlen(input)-(cmdEnd+1); //-1 to eleminate \n , +1 to eleminate the ' '
+    char *params = malloc(paramL+1);
     strcpy(params, input+cmdEnd+1);
     params[paramL]='\0';
     strncpy(cmd, input, cmdEnd);
@@ -72,20 +77,20 @@ int main(void)
     return 0;
 }
 
-char* trimString(char in[]){
-    printf("Input: %s",in);
+char* trimString(char *in){
     int start=0;
-    int end=strlen(in)-1;
-    for(start;isspace(in[start]);start++){}
-    for(end;isspace(in[end]);end--){}
-    char o[end-start+3];
-
+    int end=(int)strlen(in)-1; //-1 because you don't want to count \n
     
-    strncpy(o, in+start, end-start+3);
-    o[end-start+1] = '\n';
-    o[end-start+2] = '\0';
-    printf("getrimmter String: %s",o);
-    printf("Länge: %lu\n",strlen(o));
+    for(start;isspace(in[start]);start++){} //count up "start" to the first non-space char
+    for(end;isspace(in[end]);end--){} //count down "end" to the last non-space char
+    return strsub(in,start,(int)end);
+    }
+
+//returns the pointer to a substring from s to e (both inclusive) in a specific string
+char* strsub(char *in,const int s, const int e){
+    char *o = malloc(sizeof(char)*(e-s+2));//+1 off by one / +1 space for \0
+    strncpy(o, in+s, e-s+1); // copy everything from the input to
+    o[e-s+1] = '\0'; //add the terminator
     return o;
 }
 
