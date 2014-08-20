e30341327573079fa8cc4027470f27a488910ba3
Björn Ebbinghaus <bjebb100@hhu.de>
2014-08-20 13:06:19 +0200

    1st implementation of grep (just working with files)

--- C-project/hhush.c
+++ C-project/hhush.c
@@ -25,19 +25,17 @@
 #include <unistd.h>
 
 
-//const
+//constants
 const char* INVALID_ARGS = "invalid arguments";
 const char* INVALID_CMD = "command not found";
 const char* INVALID_DIR = "no such file or directory";
 
 //functions
-char* trimString(char *);
+void trimString(char *);
 void strsub(const char *,char *,int,int);
 struct cmd* assambleStruct(char *);
 char* grep(char*, FILE*);
-char* interpretCMD(const char *,const char *);
 char* interpretCMDstruct(struct cmd *);
-char* concat(char *,char *);
 void addHist(const char *);
 char* getLastXNodes(long);
 char* getHistory();
@@ -54,32 +52,44 @@ struct cmd {
 struct histNode {
     char* cmd;
     struct histNode *next;
+    int id;
 };
 
+//
 struct histNode *top;
 char* input;
 
 
 int main(void){
+    
     while(1){
+        
         //init row with current directory + $ symbol
-        char cwd[256]; //memory for the path
-        if(getcwd(cwd,sizeof(cwd)-1) == 0) return EXIT_FAILURE; //get current path
+        char cwd[257]; //memory for the path
+        if( getcwd( cwd,sizeof(cwd)-1 ) == 0 ) return EXIT_FAILURE; //get current path
         printf("%s $ ", cwd); //print out the path
         
         
         //read input
-        input=(char*)malloc(sizeof(char)*258); //reserve enough memory for the input
+        input = (char*)malloc(259); //reserve enough memory for the input
         fgets(input,258,stdin); //read input from console
         
         //handle input
         trimString(input);
         //addHist(input);echo abc |
         char* out = interpretCMDstruct( assambleStruct( input ) );
-        if(out != '\0') printf("%s\n",out);
+        
+        if( (*out != '\0')) {
+            if(*out != '\n'){
+                printf("%s\n",out);
+            }
+            else{
+                printf("%s",out);
+            }
+        }
         
         //free
-        free(out);
+        if(out) free(out);
         free(input);
     }
     
@@ -103,91 +113,120 @@ char* interpretCMDstruct(struct cmd *in){
         }
         else in->param = t;
     }
-
+    
+    
+    //switch / case
     if(in->cmd != NULL){
+        
         //handle "echo"
-        if ( strcmp( in->cmd, "echo" ) ==0 ){
-            ret = malloc(sizeof(in->param));
-            strcpy(ret, in->param);
+        if ( strcmp( in->cmd, "echo" ) == 0 ){
+            if(in->param != NULL){
+                ret = malloc( strlen(in->param) +1);
+                strcpy(ret, in->param);
+            }
+            else{
+                ret = malloc(1);
+                strcpy(ret, "");
+            }
         }
         
         //handle "exit"
-        else if (strcmp(in->cmd,"exit")==0){
+        else if ( strcmp(in->cmd,"exit") == 0 ) {
             if(in->param == 0) {
                 free(in);
                 free(input);
                 exit(0);
             }
             else {
-                ret = malloc(sizeof(INVALID_ARGS));
+                ret = (char*) malloc( sizeof(char*) * ( strlen(INVALID_ARGS) +1 ) );
                 strcpy(ret, INVALID_ARGS);
             }
         }
         
         //handle "date"
-        else if (strcmp(in->cmd,"date")==0){
-            if(in->param == NULL){
-                time_t time_raw = time(NULL); //get seconds since 1st Jan. 1970
-                struct tm *date = localtime(&time_raw); //transform time_raw into a struct to hold all informations about the date
-                char *d = asctime(date); //returns "date" as a formated string
-                d[strlen(d)-1]=0; //remove the trailing '\n'
-                ret = malloc(sizeof(d));
-                ret = strcpy(ret,d);
+        else if ( strcmp(in->cmd,"date") == 0 ) {
+            if(in->param == NULL) {
+                time_t time_raw = time( NULL );           //get seconds since 1st Jan. 1970
+                struct tm *date = localtime( &time_raw ); //transform time_raw into a struct to hold all informations about the date
+                char *d = asctime( date );                //returns "date" as a formated string
+                d[strlen(d)-1] = 0;                       //remove the trailing '\n'
+                ret = malloc( strlen(d) +1);
+                ret = strcpy( ret, d );
             }
             else {
-                ret = malloc(sizeof(INVALID_ARGS));
-                strcpy(ret,INVALID_ARGS);
+                ret =(char*) malloc( sizeof(char*) * (strlen(INVALID_ARGS) +1 ) );
+                strcpy(ret, INVALID_ARGS);
             }
         }
         
         //handle "cd"
-        else if (strcmp(in->cmd,"cd")==0){
-            if(chdir(in->param) == 0){
-                ret = malloc(1);
-                ret = '\0';
+        else if (strcmp(in->cmd,"cd")==0) {
+            if(in->param != NULL){
+                if(chdir(in->param) == 0) {
+                    ret = malloc(1);
+                    strcpy(ret,"");
+                }
+                else {
+                    ret = (char*) malloc( sizeof(char*) * (strlen(INVALID_DIR) +1 ) );
+                    strcpy(ret,INVALID_DIR);
+                }
             }
-            else {
-                ret = malloc(sizeof(INVALID_DIR));
-                strcpy(ret,INVALID_DIR);
+            else{
+                ret =(char*) malloc( sizeof(char*) * (strlen(INVALID_ARGS) +1 ) );
+                strcpy(ret, INVALID_ARGS);
             }
         } //change to param
         
         //handle "ls"
-        else if (strcmp(in->cmd,"ls")==0) {
-            DIR *dir = opendir ("."); //opens current directory in a dir-stream
-            char *t = "";
-            while (dir) {
-                struct dirent *d = readdir(dir); //get the next dir
-                if (d) {
-                    if (*d->d_name != '.') { //if(d_name doesn't begin with '.'){...}
-                        t = realloc(t,sizeof(t)+sizeof(d->d_name)+1);
-                        strcat(t, d->d_name);
-                        strcat(t, "\n");
+        else if ( strcmp(in->cmd,"ls")==0 ) {
+            if(in->param == NULL){
+                DIR *dir = opendir ("."); //opens current directory in a dir-stream
+                char *t = malloc(0);
+                strcpy(t,"");
+                while (dir) {
+                    struct dirent *d = readdir(dir); //get the next dir
+                    if (d) {
+                        if (*d->d_name != '.') { //if(d_name doesn't begin with '.'){...}
+                            t = realloc(t, strlen(t) + strlen(d->d_name) + 1);
+                            strcat(t, d->d_name);
+                            strcat(t, "\n");
+                        }
                     }
+                    else break;
                 }
-                else break;
+                t[strlen(t)-1] = 0; //remove the last \n
+                ret = malloc( strlen(t) + 1);
+                strcpy(ret, t);
+                free(t); //free t
             }
-            t[strlen(t)-1]=0; //remove the last \n
-            ret = malloc(sizeof(t));
-            strcpy(ret, t);
-            free(t); //free t
+            else{
+                ret = (char*) malloc( sizeof(char*) * (strlen(INVALID_ARGS) +1 ) );
+                ret = strcpy(ret, INVALID_ARGS);
+            }
         }
         
         //handle "grep"
-        else if (strcmp(in->cmd,"grep") == 0){
+        else if (strcmp(in->cmd,"grep") == 0 ) {
             FILE *file = fopen(in->param, "r");
-            grep(in->pattern, file);
+            if(file == NULL) {
+                ret = (char*) malloc( sizeof(char*) * (strlen(INVALID_DIR)+1) );
+                strcpy(ret, INVALID_DIR);
+            }
+            else{
+                ret = grep(in->pattern, file);
+                
+            }
         }
         
         //handle "history"
-        else if ( strcmp(in->cmd, "history" ) == 0) {
+        else if ( strcmp(in->cmd, "history") == 0 ) {
             if (in->param == NULL) {
                 ret = getHistory();
             }
             else {
-                if ( strcmp( in->param ,"-c" ) == 0 ) {
+                if ( strcmp(in->param ,"-c") == 0 ) {
                     clearHist();
-                    ret = malloc(1);
+                    ret = (char*) malloc( 1 * sizeof(char) );
                     ret = '\0';
                 }
                 else {
@@ -196,16 +235,18 @@ char* interpretCMDstruct(struct cmd *in){
                 //strtol for parsing String -> Int
             }
         }
+        
         else {
-            ret = malloc(sizeof(INVALID_CMD));
+            ret = (char*) malloc( sizeof(char*) * (strlen(INVALID_CMD) +1 ) );
             strcpy(ret, INVALID_CMD);
         }
     }
     
     //else:
 END:
-    free(in->param);
-    free(in);
+    if(in->param)   free(in->param);
+    if(in->pattern) free(in->pattern);
+    if(in)          free(in);
     return ret;
 }
 
@@ -225,6 +266,7 @@ struct cmd* assambleStruct(char *in){
         trimString(currentPart);
         
         nextStruct = malloc(sizeof(struct cmd));
+        nextStruct->pattern = NULL;
         nextStruct->next = currentStruct;
         currentStruct = nextStruct;
         
@@ -237,16 +279,22 @@ struct cmd* assambleStruct(char *in){
             if (strcmp( "grep", currentStruct->cmd)==0) {
                 
                 subpart = strtok(NULL," ");
-                trimString(subpart);
-                currentStruct->pattern = subpart;
                 
+                char* tmp = NULL;
+                if(subpart) {
+                    tmp = malloc( strlen(subpart) +1 );
+                    strcpy(tmp,subpart); //subpart --/--> NULL
+                }
+                currentStruct->pattern = tmp;
             }
+            
+            
             subpart = strtok(NULL,"\0");
             trimString(subpart);
             
             char* tmp = NULL;
             if(subpart) {
-                tmp = malloc(sizeof(subpart));
+                tmp = malloc( strlen(subpart) +1);
                 strcpy(tmp,subpart); //subpart --/--> NULL
             }
             currentStruct->param = tmp;
@@ -258,37 +306,42 @@ struct cmd* assambleStruct(char *in){
     return currentStruct;
 }
 
-
 char* grep(char* pattern, FILE* file){
-    char* ret = malloc(1);
-    char* line = malloc(128);
-    size_t line_size = sizeof(line);
-    while( getline(&line, &line_size, file) > 0 ) {
-        if(strstr(pattern, line) ){
-            ret = realloc(ret, sizeof(ret)+sizeof(line));
-            strcat(ret, line);
+    const int LINE_SIZE = 4096;
+    char* line = (char*)malloc( LINE_SIZE );
+    char* ret = (char*)malloc( 1 * sizeof(char) );
+    
+    while( fgets( line, LINE_SIZE, file ) ){
+        if( strstr(line, pattern) ) {
+            ret = realloc( ret, sizeof(*ret) + sizeof(*line) );
+            strcat(ret,line);
         }
     }
+    
+    free(line);
     return ret;
 }
 
 //removes the white-spaces from the beginning and the end of the string
-char* trimString(char *in){
+void trimString(char *in){
     char* ret;
+    int start;
+    int end;
+    
     if(in != NULL){
-        int start = 0;
-        int end;
-        end=(int)strlen(in)-1; //-1 because you don't want to count \n
-
-        for (; isspace(in[start]); start++) {} //count up "start" to the first non-space char
-        for (; isspace(in[end]); end--) {} //count down "end" to the last non-space char
+        start = 0;
+        end = (int)strlen(in)-1; //-1 because you don't want to count \n
         
-        ret = malloc(end+1-start);
+        while ( isspace( in[start]  ) ) start++;   //count up "start" to the first non-space char
+        while ( isspace( in[end]    ) ) end--;       //count down "end" to the last non-space char
+        
+        ret = malloc(end+1-start +1); //+1 -> \n
         strsub(in,ret,start,end+1);
+        
         strcpy(in, ret);
         free(ret);
     }
-    return NULL;
+    
 }
 
 //returns the pointer to a substring from s to e (both inclusive) in a specific string
@@ -297,12 +350,6 @@ void strsub(const char *in,char *out,const int s, const int e){
         memcpy(out, in+s, e-s); // copy everything from the input to out
         out[e-s] = '\0'; //add the terminator
     }
-}
-
-char* concat(char *a, char *b) {
-    char *r = malloc(strlen(a)+strlen(b)+1);//add 1 for \0
-    sprintf(r, "%s%s", a, b);
-    return r;
 }
 
 void addHist(const char *in) {
@@ -318,7 +365,7 @@ char* getLastXNodes(long x){
     if(x > 0) {
         struct histNode *tmp_head = top;
         for (int i=0 ; i<x && tmp_head != NULL ; i++) {
-            out = concat(out, tmp_head->cmd);
+            //out = concat(out, tmp_head->cmd);
             tmp_head = tmp_head->next;
         }
         out[strlen(out)-1] = 0;
@@ -343,13 +390,12 @@ char* getHistory(){
     c = 0;
     while(tmp_head != NULL){
         sprintf(buffer, "%d: ", c);
-        out = concat(out, buffer);
-        out = concat(out, tmp_head->cmd);
-        out = concat(out, "\n");
+        //out = concat(out, buffer);
+        //out = concat(out, tmp_head->cmd);
+        //out = concat(out, "\n");
         tmp_head = tmp_head->next;
         c++;
     }
     out[strlen(out)-1] = 0;
     return out;
 }
-
